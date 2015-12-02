09e952aadb1a497aaecdeaaffd913df97039438a
Dan Leehr <dan.leehr@duke.edu>
2015-12-02 14:14:04 -0500

    Refactor into two scripts

--- workflow_io.py
+++ workflow_io.py
@@ -46,16 +46,3 @@ def extract_image_ids(workflow):
 def print_names(image_names):
     for name in image_names:
         print name
-
-# returns list of images (names+ids) and the frozen document
-def freeze_workflow(workflow_doc, export_dir):
-    # Parse the document
-    workflow = parse_workflow(workflow_doc)
-    image_paths = extract_paths_image_ids(workflow)
-    print_names(image_paths)
-    # find the image names in the document
-    image_ids = extract_image_ids(workflow)
-    # Now consult docker API for the image IDs
-    print_names(image_ids)
-    # replace with image ids
-    
