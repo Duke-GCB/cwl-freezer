61c9f22fec1285eacad5cf793e7c6891e675a67f
Dan Leehr <dan.leehr@duke.edu>
2015-12-03 11:40:34 -0500

    cwl_freezer now exports docker images
    Next: write out workflow and see if that can be run

--- cwl_freezer.py
+++ cwl_freezer.py
@@ -1,33 +1,37 @@
 import workflow_io as wf
 import docker_io as dk
 
-from workflow_io import parse_workflow, extract_paths_image_ids, extract_image_ids, print_names, replace_values
-from docker_io import get_client, write_image, get_image_ids
+def load_workflow(workflow_doc):
+    return wf.parse_workflow(workflow_doc)
 
 # returns list of images (names+ids) and the frozen document
-def freeze_workflow(workflow_doc, export_dir):
-    # Parse the document
-    workflow = wf.parse_workflow(workflow_doc)
+def freeze_workflow(workflow): # does not parse
     image_paths = wf.extract_paths_image_ids(workflow)
-    # find the image names in the document
+
+    # Extract the docker images named in the workflow
     paths_image_ids = wf.extract_paths_image_ids(workflow)
     image_ids = wf.extract_image_ids(paths_image_ids)
-    # Now consult docker API for the image IDs
-    client = dk.get_client()
-    
-    frozen_images = {}
-    for image_id in image_ids:
-        resolved_image_ids = dk.get_image_ids(client, image_id)
-        if len(resolved_image_ids) != 1:
-            raise 'Too many images found, please be more specific'
-        frozen_images[image_id] = resolved_image_ids[0]
-    # write docker images
 
-    # Now replace workflow with frozen images
+    # Consult docker API to resolve all image names/ids to ids
+    client = dk.get_client()
+    frozen_image_metadata = {}
+    for image_id in image_ids:
+        resolved_image_metadata = dk.get_image_metadata(client, image_id)
+        if len(resolved_image_metadata) != 1:
+            raise 'Too many images found for {}, please be more specific'.format(image_id)
+        frozen_image_metadata[image_id] = resolved_image_metadata[0]
+
+    # Replace images named in workflow with resolved (frozen) ids
     for d in paths_image_ids:
         path = d[wf.PATH]
         orig_image_id = d[wf.IMAGE_ID]
-        new_image_id = frozen_images[orig_image_id]
+        new_image_id = frozen_image_metadata[orig_image_id]['Id']
         wf.replace_values(workflow, path, new_image_id)
     
-    return frozen_images, workflow
+    return frozen_image_metadata, workflow
+
+def export_images(frozen_image_metadata, export_dir):
+    # frozen_images is a dict of {'orig': 'resolved'}
+    client = dk.get_client()
+    for name, metadata in frozen_image_metadata.items():
+        dk.write_image(client, metadata, export_dir)
